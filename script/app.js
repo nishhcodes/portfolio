bf895d942cc2602ae3ba511f418e4219f12ffbe9
Issac <issacneedscoffee@gmail.com>
2024-05-09 20:09:51 +0530

    hero page completed

--- script/app.js
+++ script/app.js
@@ -1,3 +1,4 @@
+//responsive navbar starts here
 function showSidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'flex';
@@ -7,3 +8,13 @@ function hideSidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'none';
 }
+//responsive navbar ends here
+
+
+//parallax effect starts here
+window.addEventListener('scroll', function() {
+    let scrolled = window.scrollY;
+    let parallax = document.querySelector('#hero-container');
+    parallax.style.backgroundPositionY = -(scrolled * 1.8) + 'px'; 
+  });
+//parallax effect ends here
